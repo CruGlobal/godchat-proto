e0eb580892f53973260ee2254b1c584c375900b5
Tataihono Nikora <tataihono.nikora@gmail.com>
2013-10-20 12:50:41 +1300

    fronted design changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,35 +9,35 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130805074504) do
+ActiveRecord::Schema.define(version: 20130923041134) do
 
-  create_table "comments", :force => true do |t|
+  create_table "comments", force: true do |t|
     t.integer  "outsider_id"
     t.integer  "insider_id"
     t.text     "body"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "comments", ["insider_id"], :name => "index_comments_on_insider_id"
-  add_index "comments", ["outsider_id"], :name => "index_comments_on_outsider_id"
+  add_index "comments", ["insider_id"], name: "index_comments_on_insider_id", using: :btree
+  add_index "comments", ["outsider_id"], name: "index_comments_on_outsider_id", using: :btree
 
-  create_table "conversations", :force => true do |t|
+  create_table "conversations", force: true do |t|
     t.string   "topic"
     t.integer  "insider_id"
     t.integer  "outsider_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "conversations", ["insider_id"], :name => "index_conversations_on_insider_id"
-  add_index "conversations", ["outsider_id"], :name => "index_conversations_on_outsider_id"
+  add_index "conversations", ["insider_id"], name: "index_conversations_on_insider_id", using: :btree
+  add_index "conversations", ["outsider_id"], name: "index_conversations_on_outsider_id", using: :btree
 
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0
+    t.integer  "attempts",   default: 0
     t.text     "handler"
     t.text     "last_error"
     t.datetime "run_at"
@@ -45,62 +45,62 @@ ActiveRecord::Schema.define(:version => 20130805074504) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
-  create_table "languages", :force => true do |t|
+  create_table "languages", force: true do |t|
     t.string   "name"
     t.string   "locale"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "memberships", :force => true do |t|
+  create_table "memberships", force: true do |t|
     t.integer  "users_id"
     t.integer  "organizations_id"
     t.boolean  "valid"
     t.boolean  "admin"
     t.boolean  "owner"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
-  add_index "memberships", ["organizations_id"], :name => "index_memberships_on_organizations_id"
-  add_index "memberships", ["users_id"], :name => "index_memberships_on_users_id"
+  add_index "memberships", ["organizations_id"], name: "index_memberships_on_organizations_id", using: :btree
+  add_index "memberships", ["users_id"], name: "index_memberships_on_users_id", using: :btree
 
-  create_table "messages", :force => true do |t|
+  create_table "messages", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
     t.integer  "conversation_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  add_index "messages", ["conversation_id"], :name => "index_messages_on_conversation_id"
-  add_index "messages", ["user_id"], :name => "index_messages_on_user_id"
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
-  create_table "organizations", :force => true do |t|
+  create_table "organizations", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "first_name"
@@ -111,12 +111,13 @@ ActiveRecord::Schema.define(:version => 20130805074504) do
     t.string   "ip"
     t.string   "type"
     t.string   "referrer"
+    t.string   "authentication_token"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "users_languages", :force => true do |t|
+  create_table "users_languages", force: true do |t|
     t.integer "user_id"
     t.integer "language_id"
   end
