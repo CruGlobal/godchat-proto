4d0be86c4b760e46a4c69607a8fc0db60ed18a38
Josh Starcher <josh.starcher@gmail.com>
2013-11-06 16:30:26 -0500

    Add campaigns route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Chatapp::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :campaigns
-  
+
   authenticated :user do
     namespace :engineer do
       root to: "dashboard#index"
