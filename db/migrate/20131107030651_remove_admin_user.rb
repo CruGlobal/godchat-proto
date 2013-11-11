794ffc992f1664d4196b2259832907eecb778d84
Tataihono Nikora <tataihono.nikora@gmail.com>
2013-11-11 20:13:12 +1300

    bug fix for production

--- db/migrate/20131107030651_remove_admin_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminUser < ActiveRecord::Migration
-  def change
-  	drop_table :admin_users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20131107202404) do
   create_table "campaigns", force: true do |t|
     t.string   "name"
     t.string   "cname"
-    t.string   "youtube_url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "missionhub_secret"
@@ -44,8 +43,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.integer  "outsider_id"
     t.integer  "insider_id"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   add_index "comments", ["insider_id"], name: "index_comments_on_insider_id", using: :btree
@@ -55,8 +54,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.string   "topic"
     t.integer  "insider_id"
     t.integer  "visitor_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "channel"
   end
 
@@ -73,8 +72,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
@@ -82,8 +81,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
   create_table "languages", force: true do |t|
     t.string   "name"
     t.string   "locale"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "memberships", force: true do |t|
@@ -92,8 +91,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.boolean  "valid"
     t.boolean  "admin"
     t.boolean  "owner"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   add_index "memberships", ["organizations_id"], name: "index_memberships_on_organizations_id", using: :btree
@@ -103,8 +102,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.text     "body"
     t.integer  "user_id"
     t.integer  "conversation_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
@@ -112,8 +111,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
 
   create_table "organizations", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: true do |t|
@@ -133,8 +132,8 @@ ActiveRecord::Schema.define(version: 20131107202404) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "first_name"
@@ -152,34 +151,6 @@ ActiveRecord::Schema.define(version: 20131107202404) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
-  create_table "users_copy", id: false, force: true do |t|
-    t.integer  "id",                     null: false
-    t.string   "email",                  null: false
-    t.string   "encrypted_password",     null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count"
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.text     "bio"
-    t.integer  "status"
-    t.string   "location"
-    t.string   "ip"
-    t.string   "type"
-    t.string   "referrer"
-    t.string   "authentication_token"
-    t.integer  "roles_mask"
-  end
 
   create_table "users_languages", force: true do |t|
     t.integer "user_id"
